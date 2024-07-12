1ae2c3ae1dead9409a5465f4a88ddfce9e502ba9
FerushT <ferush82@gmail.com>
2024-07-12 15:47:29 +0200

    added userDate, userImagePath

--- lib/src/features/profile/domain/user_info.dart
+++ lib/src/features/profile/domain/user_info.dart
@@ -1,11 +1,16 @@
+//Bauplan für meine Eintrag-Inhalte
 class UserInfo {
   final String userName;
   final String userJob;
   final String userBio;
+  final String userDate;
+  final String userImagePath;
 
   const UserInfo({
     required this.userName,
     required this.userJob,
     required this.userBio,
+    required this.userDate,
+    required this.userImagePath,
   });
 }
