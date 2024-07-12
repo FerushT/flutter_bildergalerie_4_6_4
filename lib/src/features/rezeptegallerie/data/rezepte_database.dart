829a593120a5fed43ea1c1d4670c0d3bc8b2cf8f
FerushT <ferush82@gmail.com>
2024-07-12 15:49:31 +0200

    change bild Image to bild String

--- lib/src/features/rezeptegallerie/data/rezepte_database.dart
+++ lib/src/features/rezeptegallerie/data/rezepte_database.dart
@@ -1,39 +1,93 @@
-import 'package:flutter/material.dart';
 import 'package:flutter_bildergalerie_4_6_4/src/features/rezeptegallerie/domain/rezept.dart';
 import 'package:flutter_bildergalerie_4_6_4/src/features/rezeptegallerie/domain/zutat.dart';
 
 List<Rezept> alleRezepte = [
   Rezept(
-      title: "Lasagne",
-      description: "Italienische Spezialität",
-      zutaten: [
-        Lachs(name: "Lachs", gewicht: 1000),
-        Nudeln(name: "Nudeln", gewicht: 500),
-      ],
-      bild: Image.asset("assets/images/Lasagne.jpg")),
+    title: "Lasagne",
+    description: "Italienische Spezialität",
+    zutaten: [
+      Lachs(name: "Lachs", gewicht: 1000),
+      Nudeln(name: "Nudeln", gewicht: 500),
+    ],
+    bild: "assets/images/Lasagne.jpg",
+  ),
   Rezept(
-      title: "Chili con Carne",
-      description: "Mexikanische Spezialität",
-      zutaten: [
-        Kaese(name: "Käse", gewicht: 300),
-        Fleisch(name: "Hackfleisch", gewicht: 200),
-      ],
-      bild: Image.asset("assets/images/Chili con Carne.jpg")),
+    title: "Chili con Carne",
+    description: "Mexikanische Spezialität",
+    zutaten: [
+      Kaese(name: "Käse", gewicht: 300),
+      Fleisch(name: "Hackfleisch", gewicht: 200),
+    ],
+    bild: "assets/images/Chili con Carne.jpg",
+  ),
   Rezept(
-      title: "Pizza",
-      description: "Italienische Spezialität",
-      zutaten: [
-        Kaese(name: "Käse", gewicht: 300),
-        Getreide(name: "Getreide", gewicht: 200),
-      ],
-      bild: Image.asset("assets/images/pizza.jpg")),
+    title: "Pizza",
+    description: "Italienische Spezialität",
+    zutaten: [
+      Kaese(name: "Käse", gewicht: 300),
+      Getreide(name: "Getreide", gewicht: 200),
+    ],
+    bild: "assets/images/pizza.jpg",
+  ),
   Rezept(
     title: "Nudelauflauf",
     description: "Irgendeine Spezialität",
     zutaten: [
       Nudeln(name: "Nudeln", gewicht: 500),
-      Obst(name: "Obst", stueckzahl: 2, gewicht: 300)
+      Obst(name: "Obst", stueckzahl: 2),
     ],
-    bild: Image.asset("assets/images/Nudelauflauf.jpg"),
-  )
+    bild: "assets/images/Nudelauflauf.jpg",
+  ),
+  Rezept(
+    title: "Burger",
+    description: "mit saftigem Bauchspech, cremiger Avocado und Ahornsirup",
+    zutaten: [
+      Nudeln(name: "Hackfleisch", gewicht: 500),
+      Obst(name: "Brötchen", stueckzahl: 2),
+      Nudeln(name: "Hackfleisch", gewicht: 500),
+      Obst(name: "Brötchen", stueckzahl: 2),
+      Nudeln(name: "Hackfleisch", gewicht: 500),
+      Obst(name: "Brötchen", stueckzahl: 2),
+    ],
+    bild: "assets/images/Buerger.jpg",
+  ),
+  Rezept(
+    title: "Burger",
+    description: "mit saftigem Bauchspech, cremiger Avocado und Ahornsirup",
+    zutaten: [
+      Nudeln(name: "Hackfleisch", gewicht: 500),
+      Obst(name: "Brötchen", stueckzahl: 2),
+      Nudeln(name: "Hackfleisch", gewicht: 500),
+      Obst(name: "Brötchen", stueckzahl: 2),
+      Nudeln(name: "Hackfleisch", gewicht: 500),
+      Obst(name: "Brötchen", stueckzahl: 2),
+    ],
+    bild: "assets/images/Buerger.jpg",
+  ),
+  Rezept(
+    title: "Burger",
+    description: "mit saftigem Bauchspech, cremiger Avocado und Ahornsirup",
+    zutaten: [
+      Nudeln(name: "Hackfleisch", gewicht: 500),
+      Obst(name: "Brötchen", stueckzahl: 2),
+      Nudeln(name: "Hackfleisch", gewicht: 500),
+      Obst(name: "Brötchen", stueckzahl: 2),
+      Nudeln(name: "Hackfleisch", gewicht: 500),
+      Obst(name: "Brötchen", stueckzahl: 2),
+    ],
+    bild: "assets/images/Buerger.jpg",
+  ),
+  Rezept(
+    title: "Burger",
+    description: "mit saftigem Bauchspech, cremiger Avocado und Ahornsirup",
+    zutaten: [
+      Nudeln(name: "Hackfleisch", gewicht: 500),
+      Obst(name: "Brötchen", stueckzahl: 2),
+      Nudeln(name: "Hackfleisch", gewicht: 500),
+      Obst(name: "Brötchen", stueckzahl: 2),
+      Nudeln(name: "Hackfleisch", gewicht: 500),
+      Obst(name: "Brötchen", stueckzahl: 2),
+    ],
+    bild: "assets/images/Buerger.jpg",
+  ),
 ];
