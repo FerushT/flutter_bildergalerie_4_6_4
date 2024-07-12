f8b833cf748ba69226607a93d9583db16c2b9b88
FerushT <ferush82@gmail.com>
2024-07-12 15:50:13 +0200

    change bild Image to bild String

--- lib/src/features/rezeptegallerie/domain/rezept.dart
+++ lib/src/features/rezeptegallerie/domain/rezept.dart
@@ -1,11 +1,10 @@
-import 'package:flutter/material.dart';
 import 'package:flutter_bildergalerie_4_6_4/src/features/rezeptegallerie/domain/zutat.dart';
 
 class Rezept {
   String title;
   String description;
   List<Zutat> zutaten;
-  Image bild;
+  String bild;
 
   Rezept(
       {required this.title,
