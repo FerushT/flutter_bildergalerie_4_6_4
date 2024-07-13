8da0133b43f4afb83b69fd2bd1af2cca73b539ba
FerushT <ferush82@gmail.com>
2024-07-14 01:16:51 +0200

    add it subtitle

--- lib/src/features/rezeptegallerie/domain/rezept.dart
+++ lib/src/features/rezeptegallerie/domain/rezept.dart
@@ -2,12 +2,14 @@ import 'package:flutter_bildergalerie_4_6_4/src/features/rezeptegallerie/domain/
 
 class Rezept {
   String title;
+  String subtitle;
   String description;
   List<Zutat> zutaten;
   String bild;
 
   Rezept(
       {required this.title,
+      required this.subtitle,
       required this.description,
       required this.zutaten,
       required this.bild});
