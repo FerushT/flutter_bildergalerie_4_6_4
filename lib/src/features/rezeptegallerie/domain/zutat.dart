97da80fcf5e6585a670a6c1917a7d29ace586585
FerushT <ferush82@gmail.com>
2024-07-14 01:17:40 +0200

    add it 2 classes

--- lib/src/features/rezeptegallerie/domain/zutat.dart
+++ lib/src/features/rezeptegallerie/domain/zutat.dart
@@ -2,57 +2,69 @@ class Zutat {
   String name;
   double? gewicht;
   int? stueckzahl;
+  double? liter;
 
-  Zutat({required this.name, this.gewicht, this.stueckzahl});
+  Zutat({required this.name, this.gewicht, this.stueckzahl, this.liter});
 }
 
 //Oberkategorien
 class Fisch extends Zutat {
-  Fisch({required super.name, super.gewicht, super.stueckzahl});
+  Fisch({required super.name, super.gewicht, super.stueckzahl, super.liter});
 }
 
 class Fleisch extends Zutat {
-  Fleisch({required super.name, super.gewicht, super.stueckzahl});
+  Fleisch({required super.name, super.gewicht, super.stueckzahl, super.liter});
 }
 
 class Getreide extends Zutat {
-  Getreide({required super.name, super.gewicht, super.stueckzahl});
+  Getreide({required super.name, super.gewicht, super.stueckzahl, super.liter});
 }
 
 class Gemuese extends Zutat {
-  Gemuese({required super.name, super.gewicht, super.stueckzahl});
+  Gemuese({required super.name, super.gewicht, super.stueckzahl, super.liter});
 }
 
 class Backwaren extends Zutat {
-  Backwaren({required super.name, super.gewicht, super.stueckzahl});
+  Backwaren(
+      {required super.name, super.gewicht, super.stueckzahl, super.liter});
 }
 
 class Nudeln extends Zutat {
-  Nudeln({required super.name, super.gewicht, super.stueckzahl});
+  Nudeln({required super.name, super.gewicht, super.stueckzahl, super.liter});
 }
 
 class Gewuerze extends Zutat {
-  Gewuerze({required super.name, super.gewicht, super.stueckzahl});
+  Gewuerze({required super.name, super.gewicht, super.stueckzahl, super.liter});
 }
 
 class Kaese extends Zutat {
-  Kaese({required super.name, super.gewicht, super.stueckzahl});
+  Kaese({required super.name, super.gewicht, super.stueckzahl, super.liter});
 }
 
 class Obst extends Zutat {
-  Obst({required super.name, super.gewicht, super.stueckzahl});
+  Obst({required super.name, super.gewicht, super.stueckzahl, super.liter});
 }
 
 class Suesswaren extends Zutat {
-  Suesswaren({required super.name, super.gewicht, super.stueckzahl});
+  Suesswaren(
+      {required super.name, super.gewicht, super.stueckzahl, super.liter});
 }
 
 class Buerger extends Zutat {
-  Buerger({required super.name, super.gewicht, super.stueckzahl});
+  Buerger({required super.name, super.gewicht, super.stueckzahl, super.liter});
+}
+
+class Fluessigkeit extends Zutat {
+  Fluessigkeit(
+      {required super.name, super.gewicht, super.stueckzahl, super.liter});
+}
+
+class Salat extends Zutat {
+  Salat({required super.name, super.gewicht, super.stueckzahl});
 }
 
 //Unterkategorien....
 
 class Lachs extends Fisch {
-  Lachs({required super.name, super.gewicht, super.stueckzahl});
+  Lachs({required super.name, super.gewicht, super.stueckzahl, super.liter});
 }
