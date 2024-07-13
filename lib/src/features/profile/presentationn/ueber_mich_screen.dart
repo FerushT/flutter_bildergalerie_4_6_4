70134eb34286f1cb39a0c923c5fc754d988cbc15
FerushT <ferush82@gmail.com>
2024-07-14 01:14:55 +0200

    change UserBio

--- lib/src/features/profile/presentationn/ueber_mich_screen.dart
+++ lib/src/features/profile/presentationn/ueber_mich_screen.dart
@@ -5,7 +5,7 @@ UserInfo ferhat = const UserInfo(
     userName: "Ferhat Tüzün",
     userJob: "Hobby Koch",
     userBio:
-        "Ferhat ist ein begeisterter Hobbykoch, der seine Freizeit mit der Erkundung neuer kulinarischer Kreationen verbringt.Schon in seiner Kindheit inspiriert, kombiniert er traditionelle Rezepte mit modernen Einflüssen und legt dabei großen Wert auf frische, regionale Zutaten. Besonders liebt er mediterrane und orientalische Speisen, die er mit viel Leidenschaft und Kreativität zubereitet. Seine Freunde und Familie schätzen seine Kochkünste und die geselligen Abende, die er mit seinen Gerichten bereichert. Ferhat teilt seine kulinarischen Erlebnisse und Rezepte gerne in sozialen Medien, wo er eine begeisterte Fangemeinde hat.",
+        "Ferhat ist ein begeisterter Hobbykoch, der seine Freizeit mit der Erkundung neuer kulinarischer Kreationen verbringt.Schon in seiner Kindheit inspiriert, kombiniert er traditionelle Rezepte mit modernen Einflüssen und legt dabei großen Wert auf frische, regionale Zutaten. Besonders liebt er mediterrane und orientalische Speisen, die er mit viel Leidenschaft und Kreativität zubereitet. Seine Freunde und Familie schätzen seine Kochkünste und die geselligen Abende, die er mit seinen Gerichten bereichert. Ferhat teilt seine kulinarischen Erlebnisse und Rezepte gerne in sozialen Medien, wo er eine begeisterte Fangemeinde hat.\n\nEr experimentiert oft mit neuen Zutaten und Techniken, um einzigartige Geschmackserlebnisse zu schaffen. Ferhat besucht regelmäßig lokale Märkte, um die besten Produkte auszuwählen. Seine Küche ist ein Ort der Inspiration und Freude, in dem er immer wieder neue Gerichte kreiert. Auch das Anrichten der Speisen ist ihm wichtig, da er großen Wert auf eine ansprechende Präsentation legt. Seine Leidenschaft für das Kochen ist ansteckend und motiviert andere, ebenfalls kreativ in der Küche zu werden.",
     userDate: "12.07.2024",
     userImagePath: "assets/images/profilbild_ferhat.jpg");
 
@@ -37,14 +37,14 @@ class UeberMich extends StatelessWidget {
                 ),
                 Text(
                   ferhat.userName,
-                  style: TextStyle(fontSize: 24),
+                  style: const TextStyle(fontSize: 24),
                 ),
                 const SizedBox(
                   height: 16,
                 ),
                 Text(
                   ferhat.userJob,
-                  style: TextStyle(fontWeight: FontWeight.bold),
+                  style: const TextStyle(fontWeight: FontWeight.bold),
                 ),
                 const SizedBox(
                   height: 16,
