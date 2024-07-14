68776fb5b652147e9d3d118ec7a9631afec778c1
FerushT <ferush82@gmail.com>
2024-07-14 16:32:00 +0200

    Change Backgroundcolor

--- lib/src/features/rezeptegallerie/presentation/card_detail_screen.dart
+++ lib/src/features/rezeptegallerie/presentation/card_detail_screen.dart
@@ -12,63 +12,70 @@ class CardDetailScreen extends StatelessWidget {
         title: Text(
           "Rezeptdetails: ${rezept.title}",
         ),
-        backgroundColor: const Color.fromARGB(239, 131, 232, 98),
+        backgroundColor: const Color.fromARGB(148, 209, 178, 140),
       ),
-      body: SafeArea(
-        child: SingleChildScrollView(
-          child: Padding(
-            padding: const EdgeInsets.all(8.0),
-            child: Column(
-              mainAxisAlignment: MainAxisAlignment.start,
-              crossAxisAlignment: CrossAxisAlignment
-                  .stretch, //mit dieser funktion wollte ich bewirken, dass das Bild über das gesamte Feld gestreckt wird. Leider geht das nicht.
-              children: [
-                SizedBox(
-                  height: 400,
-                  child: Image.asset(
-                    rezept.bild,
-                    fit: BoxFit.contain,
-                  ),
-                ),
-                Padding(
-                  padding: const EdgeInsets.all(8.0),
-                  child: Text(
-                    rezept.subtitle,
-                    style: const TextStyle(
-                      fontSize: 16,
-                      fontWeight: FontWeight.bold,
+      body: Scaffold(
+        backgroundColor: const Color.fromARGB(148, 209, 178, 140),
+        body: SafeArea(
+          child: SingleChildScrollView(
+            child: Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.start,
+                crossAxisAlignment: CrossAxisAlignment
+                    .stretch, //mit dieser funktion wollte ich bewirken, dass das Bild über das gesamte Feld gestreckt wird. Leider geht das nicht.
+                children: [
+                  SizedBox(
+                    height: 400,
+                    child: Image.asset(
+                      rezept.bild,
+                      fit: BoxFit.contain,
                     ),
                   ),
-                ),
-                const SizedBox(
-                  height: 20,
-                ),
-                Padding(
-                  padding: const EdgeInsets.symmetric(horizontal: 16),
-                  child: Text(rezept.description),
-                ),
-                const Divider(
-                  indent: 16,
-                  endIndent: 16,
-                  height: 40,
-                ),
-                ListView.builder(
-                    physics: const NeverScrollableScrollPhysics(),
-                    shrinkWrap: true,
-                    itemCount: rezept.zutaten.length,
-                    itemBuilder: (context, index) {
-                      return ListTile(
-                        title: Text(
-                          rezept.zutaten[index].name,
-                        ),
-                        trailing: Text(rezept.zutaten[index].gewicht != null
-                            ? "${rezept.zutaten[index].gewicht.toString()} g"
-                            : rezept.zutaten[index].stueckzahl != null
-                                ? "${rezept.zutaten[index].stueckzahl} Stück"
-                                : "${rezept.zutaten[index].liter.toString()} Liter"),
-                      );
-                    }),
-              ],
+                  Padding(
+                    padding: const EdgeInsets.all(8.0),
+                    child: Text(
+                      rezept.subtitle,
+                      style: const TextStyle(
+                        fontSize: 16,
+                        fontWeight: FontWeight.bold,
+                      ),
+                    ),
+                  ),
+                  const SizedBox(
+                    height: 20,
+                  ),
+                  Padding(
+                    padding: const EdgeInsets.symmetric(horizontal: 16),
+                    child: Text(
+                      rezept.description,
+                      style: const TextStyle(fontSize: 16),
+                    ),
+                  ),
+                  const Divider(
+                    indent: 16,
+                    endIndent: 16,
+                    height: 40,
+                    color: Colors.grey,
+                  ),
+                  ListView.builder(
+                      physics: const NeverScrollableScrollPhysics(),
+                      shrinkWrap: true,
+                      itemCount: rezept.zutaten.length,
+                      itemBuilder: (context, index) {
+                        return ListTile(
+                          title: Text(
+                            rezept.zutaten[index].name,
+                          ),
+                          trailing: Text(rezept.zutaten[index].gewicht != null
+                              ? "${rezept.zutaten[index].gewicht.toString()} g"
+                              : rezept.zutaten[index].stueckzahl != null
+                                  ? "${rezept.zutaten[index].stueckzahl} Stück"
+                                  : "${rezept.zutaten[index].liter.toString()} Liter"),
+                        );
+                      }),
+                ],
+              ),
             ),
           ),
         ),
